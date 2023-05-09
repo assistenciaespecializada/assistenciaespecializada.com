8d2a3acbfa9922a35accf7118c74c42731f8fc93
assistencia <52335433+assistenciaespecializada@users.noreply.github.com>
2023-05-08 21:23:49 -0300

    Delete login.php

--- login.php
+++
@@ -1,6 +0,0 @@
-
-<form name="form1" method="post" action="loga.php?acao=logar">
-Login: <input type="text" name="nome"><BR>
-Senha: <input type="password" name="pwd"><BR>
-<input type="submit">
-</form
